117159323caaf330231a395aabc071c10d0e9932
Everson Soares <eversonv4@gmail.com>
2023-04-09 09:31:49 -0300

    feat: created project to register car with their specifications and bond to a user, implemented TypeORM and setting to SQLite database, creating and saving user

---
+++ src/users/users.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto) {
+    const { email, password } = body;
+
+    return await this.usersService.create(email, password);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(email: string, password: string): Promise<User> {
+    const user = this.usersRepository.create({ email, password });
+
+    return await this.usersRepository.save(user);
+  }
+}
